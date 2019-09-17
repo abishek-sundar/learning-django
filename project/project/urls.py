cf03a0cdbc3c6522c41cb8f81994e80f4501bb98
abishek-sundar <abisheksundar2@gmail.com>
2019-09-17 15:41:40 -0400

    Added some notes to follow in case I forget how to setup django again

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(urls)),
+    path('', include('blog.urls')),
 ]
