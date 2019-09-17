a6686d61c309d595c7589a163084d8efe96cee23
abishek-sundar <abisheksundar2@gmail.com>
2019-09-17 15:53:55 -0400

    Made posts appear on blog home

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'title': 'First post',
+        'date': '2nd Jan, 2019',
+        'author': 'Bob',
+        'content': 'First post woohoo'
+    },
+    {
+        'title': 'Second post',
+        'date': '3rd Jan, 2019',
+        'author': 'Jane',
+        'content': 'Second post sad'
+    }
+]
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
