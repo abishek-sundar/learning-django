70de7f72bd3e8533246885302b1eb3b000f7d291
abishek-sundar <abisheksundar2@gmail.com>
2019-09-20 13:43:14 -0400

    Added post table and some notes about basic querying

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) #auto_now=True sets it when last modified and auto_now_add=True doesn't let you change the date ever
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
